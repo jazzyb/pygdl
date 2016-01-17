48b899d60a144da4a4886c1d3b86e6d7fd0c6491
jazzyb <json.barnes@gmail.com>
2016-01-17 09:29:54 -0500

    updates datalog script

--- bin/datalog.py
+++ bin/datalog.py
@@ -10,7 +10,20 @@ QUERY = 'GDL? '
 prompt = DEFINE
 database = Database()
 
+for filename in sys.argv[1:]:
+    # read in any files from the command line
+    with open(filename, 'r') as file:
+        tokens = Lexer().lex(file=file)
+    for lit in Parser().parse(tokens).children:
+#        print(lit)
+        if lit.term == '<=':
+            head, body = lit.children[0], lit.children[1:]
+            database.define_rule(head.term, head.arity, head.children, body)
+        else:
+            database.define_fact(lit.term, lit.arity, lit.children)
+
 try:
+    # interactive loop
     while 1:
         raw_string = input(prompt).strip()
         if raw_string == '?':
@@ -22,18 +35,26 @@ try:
         elif not raw_string:
             continue
 
+#        print('-' * 20)
+#        print(raw_string)
         tokens = Lexer().lex(raw_string)
+#        print(tokens)
         try:
             tree = Parser().parse(tokens)
         except ParseError as err:
             print('error: ', err)
             continue
+#        print(tree)
+#        print('-' * 20)
 
         if prompt == QUERY:
             assert tree.arity == 1
 
             try:
                 results = database.query(tree.children[0])
+#                print('-' * 20)
+#                print(results)
+#                print('-' * 20)
             except DatalogError as err:
                 print('error: ', err)
                 continue
@@ -41,9 +62,10 @@ try:
             if type(results) == list:
                 output = ''
                 for match in results:
+                    new_string = raw_string.lower()
                     for key in match:
-                        output += raw_string.lower().replace(key, repr(match[key]))
-                    output += '\n'
+                        new_string = new_string.replace(key, repr(match[key]))
+                    output += new_string + '\n'
                 print(output)
             else:
                 print(results)
@@ -51,11 +73,15 @@ try:
         else:
             print(tree)
             for lit in tree.children:
-                if lit.term == '<=':
-                    head, body = lit.children[0], lit.children[1:]
-                    database.define_rule(head.term, head.arity, head.children, body)
-                else:
-                    database.define_fact(lit.term, lit.arity, lit.children)
+                try:
+                    if lit.term == '<=':
+                        head, body = lit.children[0], lit.children[1:]
+                        database.define_rule(head.term, head.arity, head.children, body)
+                    else:
+                        database.define_fact(lit.term, lit.arity, lit.children)
+                except DatalogError as err:
+                    print('error: ', err)
+                    break
 
 except EOFError:
     print('')
